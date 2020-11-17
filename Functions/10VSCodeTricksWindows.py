a36cfbfe4853af58570c12486f8319d6f6cabf6f
A.J. Boilanger <ajboilanger@gmail.com>
2020-11-17 18:52:14 -0500

    More function files from Coding with Mosh

---
+++ Functions/10VSCodeTricksWindows.py
@@ -0,0 +1,6 @@
+# End/Home keys to move to end/start of line.
+# CTRL + End/Home to move to top/bottom of file.
+# Alt + up/down arrow to move lines up and down
+# Shift + Alt + Down arrow will duplicate highlighted line
+# Ctrl + / will comment selected text
+# You can use autocomplete to enter variable names quickly.
