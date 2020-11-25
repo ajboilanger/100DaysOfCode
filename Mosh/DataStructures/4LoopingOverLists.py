b02c44ffaf358e15c358b635498b5e9f1bc34674
A.J. Boilanger <ajboilanger@gmail.com>
2020-11-25 00:48:25 -0500

    Added more code explanation in comments

--- Mosh/DataStructures/4LoopingOverLists.py
+++ Mosh/DataStructures/4LoopingOverLists.py
@@ -1,7 +1,13 @@
-letters = ["a", "b", "c"]
+letters = ["a", "b", "c"]  # this creates a list of the letters a, b, and c.
 # for letter in letters:
 #     print(letter)
 # for letter in enumerate(letters):
 #     print(letter)
+# this iterates over "letters" and returns each index, and the letter associated with it
 for index, letter in enumerate(letters):
-    print(index, letter)
+    print(index, letter)  # then the index and letter are printed
+
+# the print statement returns the following:
+# 0 a
+# 1 b
+# 2 c
