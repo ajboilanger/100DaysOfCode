b24aeb38fbe28ad0ef0c41bd9709a17fac51091a
A.J. Boilanger <ajboilanger@gmail.com>
2020-12-06 20:27:25 -0500

    Learning to handle exceptions.

---
+++ Mosh/6Exceptions/2HandlingExceptions.py
@@ -0,0 +1,9 @@
+try:
+    age = int(input("Age: "))
+except ValueError as ex:
+    print("You did not enter a valid age.")
+    print(ex)
+    print(type(ex))
+else:
+    print("No exceptions were found.")
+print("Execution continues.")
