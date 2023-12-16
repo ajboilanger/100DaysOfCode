f1bb14da6cdd4a1dbba86dac5f26f20503679b51
Arthur Boilanger <ajboilanger@gmail.com>
2023-12-15 19:39:59 -0500

    Add print for city/pet name after input.

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,4 +1,6 @@
 print("Welcome to the Band Name Generator!")
 city_name = input("What is the name of the city you grew up in?")
+print(city_name)
 pet_name = input("What is the name of your pet?")
+print(pet_name)
 print("Your band name could be " + city_name + " " + pet_name)
